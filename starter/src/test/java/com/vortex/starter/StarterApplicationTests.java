fb1c0ad490482a8cebd8e96d8b699ccdbeeb2245
Hovhannes Nazaryan <hovnaz.arm@gmail.com>
2022-11-03 16:36:36 +0400

    initial commit create starter module

---
+++ starter/src/test/java/com/vortex/starter/StarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vortex.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
