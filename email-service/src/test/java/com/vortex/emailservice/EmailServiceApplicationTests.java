a158b2caea01310de88d21c3c96e082d99255925
Hovhannes Nazaryan <hovnaz.arm@gmail.com>
2022-11-03 16:28:43 +0400

    initial commit create email service module

---
+++ email-service/src/test/java/com/vortex/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vortex.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
